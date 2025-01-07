c187a71f265836efdd0a89c05e1a86f4e322dd4b
Kamil Kubiczek <kamil.kubiczek7@gmail.com>
2025-01-07 17:07:26 +0100

    refactor: Update package.ts exports
    Refactor the package.ts file to update the exports. Added additional types for the tRPC client used outside of the repository.

--- package.ts
+++ package.ts
@@ -2,4 +2,22 @@
  * This file exports types for private npm package used by tRPC client outside of this repository.
  */
 
-export type { AppRouter } from "./routes"
+export type {
+   AppRouter,
+   CreateExpenseRouterInput,
+   CreateExpenseRouterOutput,
+   DeleteExpenseRouterInput,
+   DeleteExpenseRouterOutput,
+   GetExpenseRouterInput,
+   GetExpenseRouterOutput,
+   LoginRouterInput,
+   LoginRouterOutput,
+   RegisterRouterInput,
+   RegisterRouterOutput,
+   LogoutRouterInput,
+   LogoutRouterOutput,
+   RefreshTokenRouterInput,
+   RefreshTokenRouterOutput,
+   UpdateExpenseRouterInput,
+   UpdateExpenseRouterOutput
+} from "./routes"
